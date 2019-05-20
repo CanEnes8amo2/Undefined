9573f4eccf3c013672758f9a37b5a3960d006b39
Enes Can <47444407+Enes1039335@users.noreply.github.com>
2019-05-20 12:10:53 +0400

    Created basic form.

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-$error = "";
+// $error = "";
 if(isset($_POST['submit'])) {
     if(!empty($_POST['username']) && !empty($_POST['password'])) {
         require("formdb.php");
         $username = trim($_POST['username']);
         $password = md5($_POST['password']);
       
-        $sql = "SELECT * FROM gebruikers WHERE username = '". $username."'";
+        $sql = "SELECT * FROM gebruikers WHERE username = '". $username."'AND password = '". $password."'";
         
         if($result = $conn->query($sql)) {
             $aantal = $result->num_rows;
@@ -17,12 +17,16 @@ if(isset($_POST['submit'])) {
                 $_SESSION['ingelogd'] = true;
                 header("Location: ingelogddb.php");
             } else {
-                $error = "Username or password incorrect.";
+                // $error = "Username or password incorrect.";
+                echo '<script language="javascript">';
+                echo 'alert("Username or password incorrect.")';
+                echo '</script>';
             }
         }
-    } else {
-        $error = "Username en password zijn verplicht.";
     } 
+    // else {
+    //     $error = "Username en password zijn verplicht.";
+    // } 
 }
 ?>
 
@@ -129,7 +133,6 @@ if(isset($_POST['submit'])) {
                         <div class="form-group">
                             <input type="submit" name="submit" class="btn btn-primary btn-block btn-lg" value="Login">
                         </div>
-                        <?php echo $error; ?>
                     </form>				
                 </div>
                 <div class="modal-footer">
--- ingelogddb.php
+++ ingelogddb.php
@@ -1,9 +1,41 @@
 <?php
-    session_start();
-    if (isset($_SESSION['ingelogd'])) {
-        echo $_SESSION['password'];
-    } else {
-        header("Location: index.php");
-        // header("Location: http://myhost.com/mypage.php");
-    }
+session_start();
+if (isset($_SESSION['ingelogd'])) {
+    // echo $_SESSION['password'];
+} else {
+    header("Location: index.php");
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Database</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <form action="insert.php" method="post" class="border border-light p-5" >
+        <p class="h4 mb-4 text-center">Insert Into Database</p>
+        <input type="text" id="defaultContactFormName" class="form-control mb-4" placeholder="Name" autocomplete="off" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAkCAYAAADo6zjiAAAAAXNSR0IArs4c6QAAAbNJREFUWAntV8FqwkAQnaymUkpChB7tKSfxWCie/Yb+gbdeCqGf0YsQ+hU95QNyDoWCF/HkqdeiIaEUqyZ1ArvodrOHxanQOiCzO28y781skKwFW3scPV1/febP69XqarNeNTB2KGs07U3Ttt/Ozp3bh/u7V7muheQf6ftLUWyYDB5yz1ijuPAub2QRDDunJsdGkAO55KYYjl0OUu1VXOzQZ64Tr+IiPXedGI79bQHdbheCIAD0dUY6gV6vB67rAvo6IxVgWVbFy71KBKkAFaEc2xPQarXA931ot9tyHphiPwpJgSbfe54Hw+EQHMfZ/msVEEURjMfjCjbFeG2dFxPo9/sVOSYzxmAwGIjnTDFRQLMQAjQ5pJAQkCQJ5HlekeERxHEsiE0xUUCzEO9AmqYQhiF0Oh2Yz+ewWCzEY6aYKKBZCAGYs1wuYTabKdNNMWWxnaA4gp3Yry5JBZRlWTXDvaozUgGTyQSyLAP0dbb3DtQlmcan0yngT2ekE9ARc+z4AvC7nauh9iouhpcGamJeX8XF8MaClwaeROWRA7nk+tUnyzGvZrKg0/40gdME/t8EvgG0/NOS6v9NHQAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
+        <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" placeholder="Message"></textarea>
+        <div class="input-group mb-4">
+            <div class="input-group-prepend">
+                <span class="input-group-text">Upload</span>
+            </div>
+            <div class="custom-file">
+                <input type="file" class="custom-file-input" id="fileInput" aria-describedby="fileInput">
+                <label class="custom-file-label" for="fileInput">File Label</label>
+            </div>
+        </div>
+        <button class="btn btn-info btn-block" type="submit">Send</button>
+    </form>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
